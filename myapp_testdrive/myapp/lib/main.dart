16d96d6844b4a12f75915e2628515ec6f0b3dce6
Derick Chou <derick.chou@fluxconn.com>
2020-11-03 18:07:27 +0800

    Add new widgets

--- myapp_testdrive/myapp/lib/main.dart
+++ myapp_testdrive/myapp/lib/main.dart
@@ -1,22 +1,61 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import './question.dart';
+import './answer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbits', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'Who\'s your favorite showgirl?',
+      'answers': ['kiki', 'lala', 'lulu', 'meowmeow'],
+    }
+  ];
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    if (_questionIndex < questions.length) {}
+    print(_questionIndex);
+  } // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Welcome to Flutter',
+        title: 'My First Flutter',
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Welcome to Flutter'),
-          ),
-          body: Center(
-            child: Text('Hello World'),
-          ),
-        ));
+            appBar: AppBar(
+              title: Question('My First App'),
+            ),
+            body: Column(
+              children: [
+                Question(
+                  questions[_questionIndex]['questionText'],
+                ),
+                ...(questions[_questionIndex]['answers'] as List<String>)
+                    .map((answer) {
+                  return Answer(_answerQuestion, answer);
+                }).toList()
+              ],
+            ) //Column
+            ));
   }
 }
